764067725255aee0c524159f37a6e509252d73f1
WhaleK233 <wjy2357036573@gmail.com>
2024-11-05 19:29:14 +0800

    小修改

--- Red Panda Bazaar Code/ModEntry.cs
+++ Red Panda Bazaar Code/ModEntry.cs
@@ -3,7 +3,6 @@ using Red_Panda_Bazaar_Code.Config;
 using Red_Panda_Bazaar_Code.VisualEffects;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewValley;
 
 namespace Red_Panda_Bazaar_Code;
 
@@ -11,14 +10,14 @@ public class ModEntry : Mod
 {
     private ModConfig Config { get; set; } = null;
 
-    private IModHelper Helper { get; set; } = null;
+    private IModHelper Helper2 { get; set; } = null;
 
     public override void Entry(IModHelper helper)
     {
         Monitor.Log($"Red Panda Bazaar Code Initializing...", LogLevel.Debug);
 
-        Helper = helper;
-        Config = this.Helper.ReadConfig<ModConfig>();
+        Helper2 = helper;
+        Config = this.Helper2.ReadConfig<ModConfig>();
 
         helper.Events.GameLoop.GameLaunched += OnGameLaunched;
     }
@@ -28,8 +27,8 @@ public class ModEntry : Mod
         // 初始化通用模组配置菜单
         InitializeGenericModConfigMenu(sender, e);
 
-        FireFlyEffects.Enable(Helper, Monitor, Config);
-        WeatherController.Enable(Helper, Monitor, Config);
+        FireFlyEffects.Enable(Helper2, Monitor, Config);
+        WeatherController.Enable(Helper2, Monitor, Config);
     }
 
     #region Generic Mod Config Menu
@@ -37,27 +36,27 @@ public class ModEntry : Mod
     private void InitializeGenericModConfigMenu(object sender, GameLaunchedEventArgs e)
     {
         // 获取通用模组配置菜单的API
-        var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>(ModCompat.GenericModConfigMenu);
+        var configMenu = this.Helper2.ModRegistry.GetApi<IGenericModConfigMenuApi>(ModCompat.GenericModConfigMenu);
         if (configMenu is null) return;
 
         // 注册模组
         configMenu.Register(
             mod: this.ModManifest,
             reset: () => Config = new ModConfig(),
-            save: () => this.Helper.WriteConfig(Config)
+            save: () => this.Helper2.WriteConfig(Config)
         );
 
         // 添加选项
         configMenu.AddBoolOption(
             mod: this.ModManifest,
-            name: () => this.Helper.Translation.Get("enabled"),
+            name: () => this.Helper2.Translation.Get("enabled"),
             getValue: () => Config.Enabled,
             setValue: value => Config.Enabled = value
         );
 
         configMenu.AddNumberOption(
             mod: this.ModManifest,
-            name: () => this.Helper.Translation.Get("number-of-firefly"),
+            name: () => this.Helper2.Translation.Get("number-of-firefly"),
             getValue: () => Config.NumberOfFireFly,
             setValue: value => Config.NumberOfFireFly = value,
             min: 0,
