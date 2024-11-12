a6900ca65918df86bc665d6a08e2988b9d5d8a58
WhaleK233 <wjy2357036573@gmail.com>
2024-11-12 18:14:25 +0800

    赌徒帽子附带速度1buff

--- Red Panda Bazaar Code/Buffs/BuffController.cs
+++ Red Panda Bazaar Code/Buffs/BuffController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Red_Panda_Bazaar_Code.Utils;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
+using StardewValley.Buffs;
 
 namespace Red_Panda_Bazaar_Code.Buffs;
 
@@ -17,8 +20,28 @@ public static class BuffController
         {
             InitCustomBuffs();
 
+            Tools.Helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+
             Enabled = true;
             Tools.Log("Custom Buffs Enabled");
+        }
+    }
+
+    private static void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
+    {
+        if (Context.IsWorldReady && Context.IsGameLaunched)
+        {
+            if (Game1.player.hat?.Get()?.Name == Hat.GamblerHat)
+            {
+                Game1.player.applyBuff(new Buff(
+                    id: "speed",
+                    duration: 100,
+                    effects: new BuffEffects()
+                    {
+                        Speed = { 1 }
+                    }
+                ));
+            }
         }
     }
 
@@ -29,7 +52,7 @@ public static class BuffController
             iconTexture: Tools.Helper.ModContent.Load<Texture2D>("assets/RedPandaBazaar_Buffs.png"),
             iconSheetIndex: 0,
             duration: Buff.ENDLESS,
-            effects: new StardewValley.Buffs.BuffEffects()
+            effects: new BuffEffects()
             {
                 Speed = { 2 },
                 Defense = { 4 },
@@ -45,5 +68,10 @@ public static class BuffController
     public static class Food
     {
         public const string Golden_Delight = "RedPandaBazaar_Golden_Delight";
+    }
+
+    public static class Hat
+    {
+        public const string GamblerHat = "RedPandaBazaar_GamblerHat";
     }
 }
