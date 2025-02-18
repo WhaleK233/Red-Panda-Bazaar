a87d5f4cfe959184b027b2be62bc6c23db89156f
WhaleK233 <wjy2357036573@gmail.com>
2025-02-18 21:29:42 +0800

    添加自动更新版本号的脚本

---
+++ Red Panda Bazaar Code/scripts/UpdateVersion.py
@@ -0,0 +1,26 @@
+﻿import json
+import os
+
+try:
+    manifest_path = "manifest.json"
+    if not os.path.exists(manifest_path):
+        print(f"Error! {manifest_path} not found!")
+        exit(1)
+
+    with open(manifest_path, "r") as f:
+        manifest = json.load(f)
+
+    version = manifest["Version"].split("-")
+    ver, build = map(str, version)
+    b, num = build.split(".")
+    num = str(int(num) + 1)
+    new_version = f"{ver}-{b}.{num}"
+
+    manifest["Version"] = new_version
+    with open(manifest_path, "w") as f:
+        json.dump(manifest, f, indent=2)
+
+    print(f"Update Version: {new_version}")
+
+except Exception as e:
+    print(f"{e}")
